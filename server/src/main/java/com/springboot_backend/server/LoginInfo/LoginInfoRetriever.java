05c060b70ceb3173d1a7b0ef24a2f31b3252161b
AshtonGlover <113224225+AshtonGlover@users.noreply.github.com>
2024-08-21 19:58:02 -0400

    feat: untracked server

--- server/src/main/java/com/springboot_backend/server/LoginInfo/LoginInfoRetriever.java
+++
@@ -1,6 +0,0 @@
-package com.springboot_backend.server.LoginInfo;
-
-public class LoginInfoRetriever {
-    public static String username = "username";
-    public static String password = "password";
-}
